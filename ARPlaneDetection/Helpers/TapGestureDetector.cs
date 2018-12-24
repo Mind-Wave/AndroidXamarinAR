3d01b85f5e3417fc0ba05551554dd5e9aab763e5
Serhii Bilovol <serhiibilovol@gmail.com>
2018-12-24 15:53:35 +0200

    Initial commit

---
+++ ARPlaneDetection/Helpers/TapGestureDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.Views;
+
+namespace ARPlaneDetection.Helpers
+{
+    class TapGestureDetector : GestureDetector.SimpleOnGestureListener
+    {
+        public Func<MotionEvent, bool> SingleTapUpHandler { get; set; }
+
+        public override bool OnSingleTapUp(MotionEvent e)
+        {
+            return SingleTapUpHandler?.Invoke(e) ?? false;
+        }
+
+        public Func<MotionEvent, bool> DownHandler { get; set; }
+
+        public override bool OnDown(MotionEvent e)
+        {
+            return DownHandler?.Invoke(e) ?? false;
+        }
+    }
+}
